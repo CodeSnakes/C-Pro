4fc59839fa775ca6cf7a3a711c939d8dd2b16c77
伊长空 <yichangkong@outlook.com>
2021-04-07 10:34:56 +0800

    【更新配置文件】-【新增输入输出标准库使用方法、整型类型】

--- Basic/Hello.cpp
+++ Basic/Hello.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#define PI 3.1415
+// 字符常量
 using namespace std;
-
+const float pi = 3.1415;
+// 常变量
+// 有些编译系统还未实现C99的功能 ，因此不能使用常变量
 int main()
 {
+    // 一个字节通常8位长。 一个位节存储8位无符号数，储存的数值范围为0-255
     cout << "Hello, World C++!";
     return 0;
 }
---
+++ Basic/printf.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+    int number;
+    number = 2021;
+    /*一个简单的C程序注释*/
+    printf("Hello ! I am yichangkong\n");
+    printf("This year is %d\n", number);
+    /* 笔者自述：
+    时间：18：24
+    地点：北半球
+    天气：窗外天气不明 微冷 肚子有点饿
+*/
+    return 0;
+}
