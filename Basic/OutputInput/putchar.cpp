c9eac114ab2e8be52947769d57d9d08853ac8df2
伊长空 <yichangkong@outlook.com>
2021-04-15 14:09:57 +0800

    push
    【新增Variables
    变量文件夹】

---
+++ Basic/OutputInput/putchar.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    char a = 'B', b = 'O', c = 'Y';
+    putchar(a);
+    putchar(b);
+    putchar(c);
+    putchar('\n');
+    int A = 66, B = 79, C = 89;
+    putchar(A);
+    putchar(B);
+    putchar(C);
+    putchar('\n');
+    return 0;
+}
