3cd4f92f04fe881b8758bff60adfc3cd8592823d
伊长空 <yichangkong@outlook.com>
2021-05-06 17:20:22 +0800

    [Union Modle] [ eaxmpleUnion ]

---
+++ Basic/BuildDataTypes/union/simpleUnion.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
+union simpleUnion
+{
+    int i;
+    char ch;
+    float f;
+};
